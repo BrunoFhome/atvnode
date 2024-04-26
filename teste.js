b4bbdd8346f99c280d4214da66556e4edb48426c
BrunoFhome <124011928+BrunoFhome@users.noreply.github.com>
2024-04-26 20:34:27 -0300

    Atividade node

---
+++ teste.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const requestListener = function (req, res) {
+if (req.url === "/") { // Se o URL da solicitação for "/", retorna "Você está na página inicial"
+res.writeHead(200);
+res.end("Você está na página inicial");
+} else if (req.url === "/sobre") { // Se o URL da solicitação for "/sobre", retorna "Você está na página sobre"
+res.writeHead(200);
+
+fs.readFile('sobre.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+        res.end('Pagina nao encontrada');
+    
+    }
+    console.log(data);
+    res.end(data);
+});
+
+} else { // Para qualquer outro URL, retorna "Página não encontrada"
+res.writeHead(404);
+res.end("Página não encontrada");
+}
+};
+
+const server = http.createServer(requestListener);
+server.listen(8000, 'localhost', () => {
+console.log("Servidor está rodando em http://localhost:8000");
+});
